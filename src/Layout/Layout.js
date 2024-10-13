7ad7853cd801b3fd34065dc6ca40b24810331de3
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-10-13 20:04:48 +1000

    added content borders

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -3,10 +3,11 @@ import { Outlet } from "react-router-dom";
 import Header from "../Layout/Header";
 import Navigation from "../Layout/Navigation";
 import Footer from "../Layout/Footer";
+import "./Layout.css";
 
 export default function Layout() {
   return (
-    <div className="body">
+    <div className="page-body">
       <Header />
       <div className="main-content-container">
         <main className="main-content">
