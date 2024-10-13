58ba9168edea54539a313722c88fd9d43019c820
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-10-13 20:04:08 +1000

    added home content

--- src/Content/Home.js
+++ src/Content/Home.js
@@ -1,4 +1,15 @@
 import React from "react";
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home-container">
+      <h1>Hi There.</h1>
+      <p>
+        My name is Susanna, I am a front-end and aspiring backend developer from
+        Germany, based in Australia. My coding journey started a about a year
+        ago, with no end in side. I love to learn and am looking to eventually
+        be able to build software that can make a change, implementing my
+        knowledge from previos professions and studies as well.{" "}
+      </p>
+    </div>
+  );
 }
