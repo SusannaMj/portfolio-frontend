99c9a5a6f08e0a014a44256a47b705eb7b9c8ad4
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-10-13 23:32:39 +1000

    added styling to home content

--- src/Content/Home.js
+++ src/Content/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Home.css";
 export default function Home() {
   return (
     <div className="home-container">
-      <h1>Hi There.</h1>
-      <p>
+      <h1
+        style={{ fontSize: "116px", fontWeight: "100", marginBottom: "20px" }}
+      >
+        Hi There.
+      </h1>
+      <p style={{ lineHeight: "1.6", fontSize: "17px" }}>
         My name is Susanna, I am a front-end and aspiring backend developer from
         Germany, based in Australia. My coding journey started a about a year
         ago, with no end in side. I love to learn and am looking to eventually
