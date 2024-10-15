89fc88b747bbfb6a795bada4b8ab70f5ae736c44
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-10-15 22:22:16 +1000

    styled header (contact links and homepage link)
    - added animation to homepage (around Y is not centered, need to review)

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,30 +1,44 @@
 import React from "react";
 import "../Layout/Header.css";
+import { Link } from "react-router-dom";
 import { FaEnvelope } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { FaHouseUser } from "react-icons/fa";
 
 export default function Header() {
   return (
     <header className="contact">
-      <a href="#" rel="noreferrer" target="_blank">
-        <FaGithub
-          style={{ color: "var(--grey-white)" }}
-          className="contact-icon"
-        />
-      </a>
-      <a href="#" rel="noreferrer" target="_blank">
-        <FaEnvelope
-          style={{ color: "var(--grey-white)" }}
-          className="contact-icon"
-        />
-      </a>
-      <a href="#" rel="noreferrer" target="_blank">
-        <FaLinkedinIn
-          style={{ color: "var(--grey-white)" }}
-          className="contact-icon"
-        />
-      </a>
+      <div className="home-icon" style={{ paddingLeft: "10px" }}>
+        <Link to="/">
+          <FaHouseUser
+            style={{
+              color: "var(--grey-white)",
+            }}
+            className="contact-icon"
+          />
+        </Link>
+      </div>
+      <div className="contact-right" style={{ marginRight: "40px" }}>
+        <a href="#" rel="noreferrer" target="_blank">
+          <FaGithub
+            style={{ color: "var(--grey-white)" }}
+            className="contact-icon"
+          />
+        </a>
+        <a href="#" rel="noreferrer" target="_blank">
+          <FaEnvelope
+            style={{ color: "var(--grey-white)" }}
+            className="contact-icon"
+          />
+        </a>
+        <a href="#" rel="noreferrer" target="_blank">
+          <FaLinkedinIn
+            style={{ color: "var(--grey-white)" }}
+            className="contact-icon"
+          />
+        </a>
+      </div>
     </header>
   );
 }
