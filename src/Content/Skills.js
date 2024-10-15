ee784466a30b0dc15df7e69c32dd4b6e063a452e
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-10-15 23:47:10 +1000

    added grid structure and design

--- src/Content/Skills.js
+++ src/Content/Skills.js
@@ -1,4 +1,27 @@
 import React from "react";
+import "./Skills.css";
 export default function Skills() {
-  return <div>Skills</div>;
+  return (
+    <div className="skills">
+      <div className="coding-skills">
+        <div className="skill">HTML</div>
+        <div className="skill">CSS</div>
+        <div className="skill">JavaScript</div>
+        <div className="skill">React.js</div>
+        <div className="skill">Responsive Design</div>
+        <div className="skill">Bootstrap</div>
+        <div className="skill">API integration</div>
+        <div className="skill">AI integration</div>
+      </div>
+      <div className="soft-skills">
+        <div className="skill">Problem Solving</div>
+        <div className="skill">Logical Thinking</div>
+        <div className="skill">Time Management</div>
+        <div className="skill">Organised</div>
+        <div className="skill">Self-motivated</div>
+        <div className="skill">Research</div>
+      </div>
+      <a href="https://www.freepik.com/search">Icon by Freepik</a>
+    </div>
+  );
 }
