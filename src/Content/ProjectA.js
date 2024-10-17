b88800215e32e1109ed84296b391779e066ff640
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-10-17 23:40:04 +1000

    added and styled project page

--- src/Content/ProjectA.js
+++ src/Content/ProjectA.js
@@ -1,5 +1,22 @@
 import React from "react";
+import "./ProjectA.css";
+import petProject from "../images/pet-project-homepage.png";
 
 export default function ProjectA() {
-  return <div>ProjectA</div>;
+  return (
+    <div>
+      <h4 style={{ fontSize: "50px", marginTop: "0" }}>Pet-adoption-App</h4>
+      <div className="project-container">
+        <img
+          className="project-picture"
+          src={petProject}
+          style={{ borderRadius: "15px" }}
+        />
+        <div className="project-description">
+          A responsive web application built to find your new best friend, with
+          the help of two different API´s and Open AI.{" "}
+        </div>
+      </div>
+    </div>
+  );
 }
