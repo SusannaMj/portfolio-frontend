0d66be9182d15ff75332acc77eb909b74a971021
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-10-19 22:53:42 +1000

    added content, adjusted class naming to specify projects in css

--- src/Content/ProjectA.js
+++ src/Content/ProjectA.js
@@ -6,13 +6,13 @@ export default function ProjectA() {
   return (
     <div>
       <h4 style={{ fontSize: "50px", marginTop: "0" }}>Pet-adoption-App</h4>
-      <div className="project-container">
+      <div className="projectA-container">
         <img
-          className="project-picture"
+          className="projectA-picture"
           src={petProject}
           style={{ borderRadius: "15px" }}
         />
-        <div className="project-description">
+        <div className="projectA-description">
           A responsive web application built to find your new best friend, with
           the help of two different API´s and Open AI.{" "}
         </div>
--- src/Content/ProjectB.js
+++ src/Content/ProjectB.js
@@ -1,5 +1,23 @@
 import React from "react";
 
+import "./ProjectB.css";
+import gymWebsite from "../images/gym-website.png";
+
 export default function ProjectB() {
-  return <div>ProjectB</div>;
+  return (
+    <div>
+      <h4 style={{ fontSize: "50px", marginTop: "0" }}>Pet-adoption-App</h4>
+      <div className="projectB-container">
+        <div className="projectB-description">
+          A responsive web application, built to play around with all thing CSS,
+          creating an interactive website showcasing a gym.{" "}
+        </div>{" "}
+        <img
+          className="projectB-picture"
+          src={gymWebsite}
+          style={{ borderRadius: "15px" }}
+        />
+      </div>
+    </div>
+  );
 }
